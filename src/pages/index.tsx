dcf8e14a603046ddcd6033c421a20f4d8d351d84
CalebeSG <calebeguimaraes10@gmail.com>
2022-08-11 09:30:11 -0300

    created main contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,62 @@
+import Head from 'next/head'
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia - Sujeito Programador</title>
+      </Head>
+
+      <main>
+        <div className={styles.banner}>
+          <section>
+            <h1>Levando você ao próximo nível!</h1>
+            <span>
+              Uma plataforma com cursos que vão do zero até o profissional na
+              pratica, direto ao ponto aplicando o que usamos no mercado de
+              trabalho. 👊
+            </span>
+            <a type="button" href="/">
+              Começar agora!
+            </a>
+          </section>
+
+          <img
+            src="/images/banner-conteudos.png"
+            alt="Conteúdos sujeito programador"
+          />
+        </div>
+
+        <hr className={styles.divider} />
+
+        <div className={styles.wrapper}>
+          <section>
+            <h2>Aprenda criar aplicativos para Android e iOS</h2>
+            <span>
+              Você vai descobrir o jeito mais moderno de desenvolver apps
+              nativos para iOS e Android, construindo aplicativos do zero até
+              aplicativos.
+            </span>
+          </section>
+
+          <img src="/images/financasApp.png" alt="Aplicativo de finanças" />
+        </div>
+
+        <hr className={styles.divider} />
+
+        <div className={styles.wrapper}>
+          <img src="/images/webDev.png" alt="Aplicativo web" />
+
+          <section>
+            <h2>Aprenda criar sistemas web</h2>
+            <span>
+              Criar sistemas web, sites usando as tecnologias mais modernas e
+              requisitadas pelo mercado.
+            </span>
+          </section>
+        </div>
+      </main>
+    </>
   )
 }
